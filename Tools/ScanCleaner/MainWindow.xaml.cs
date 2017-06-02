24613c0418e05adf8d501c171d73ce7cb4349779
AgentMC <agentmc@mail.ru>
2017-06-02 18:48:31 +0100

    Initial cleaner source code

---
+++ Tools/ScanCleaner/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+using Path = System.IO.Path;
+
+namespace ScanCleaner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private BitmapImage _baseImage;
+        private SimpleBitmap _wrapper;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Load(object sender, RoutedEventArgs e)
+        {
+            var ofd = new OpenFileDialog {Filter = "Jpeg|*.jpg"};
+            if (ofd.ShowDialog() != true) return;
+            var path = ofd.FileName;
+            var fn = Path.GetFileNameWithoutExtension(path);
+            var idx = int.Parse(fn.Split('_')[1]);
+            OffsetBox.Text = idx%2 == 0 ? "257" : "270";
+            _baseImage = new BitmapImage(new Uri(path, UriKind.Absolute));
+            Reload(sender, e);
+            Title = path;
+            ApplySpecific(false);
+        }
+
+        private void Reload(object sender, RoutedEventArgs e)
+        {
+            _wrapper = new SimpleBitmap(_baseImage);
+            Viewport.Source = _wrapper.BaseBitmap;
+        }
+
+        private void Apply(object sender, RoutedEventArgs e)
+        {
+            ApplySpecific(false);
+        }
+
+        private void ApplySpecific(bool alwaysWhite)
+        {
+            var offset = int.Parse(OffsetBox.Text);
+            var skip = int.Parse(SkipBox.Text);
+            var white = int.Parse(CleanBox.Text);
+            var color = (alwaysWhite || (IsBlack.IsChecked == false)) ? Colors.White : Colors.Black;
+            _wrapper.BaseBitmap.Lock();
+            for (int i = offset; i < _baseImage.PixelWidth; i += (skip + white))
+            {
+                if (i + skip > _baseImage.PixelWidth) continue;
+                for (int j = 0; j < white; j++)
+                {
+                    for (int k = offset + skip; k < _baseImage.PixelHeight; k++)
+                    {
+                        var x = i + skip + j;
+                        if (x >= _baseImage.PixelWidth) continue;
+                        _wrapper.SetColor(i + skip + j, k, color, false);
+                    }
+                }
+                _wrapper.BaseBitmap.AddDirtyRect(new Int32Rect(i + skip,
+                                                               0,
+                                                               Math.Min(white, _baseImage.PixelWidth - (i + skip)),
+                                                               _baseImage.PixelHeight));
+            }
+            _wrapper.BaseBitmap.Unlock();
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            ApplySpecific(true);
+            var path = _baseImage.UriSource.OriginalString.Replace("sao18", "sao18Cleanup");
+            var encoder = new JpegBitmapEncoder() {QualityLevel = 100};
+            encoder.Frames.Add(BitmapFrame.Create(_wrapper.BaseBitmap));
+            using (var writer = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite,FileShare.ReadWrite))
+                encoder.Save(writer);
+            ApplySpecific(false);
+        }
+
+        private void OffsetPlus(object sender, RoutedEventArgs e)
+        {
+            OffsetMove(sender, e, 10);
+        }
+
+        private void OffsetMinus(object sender, RoutedEventArgs e)
+        {
+            OffsetMove(sender, e, -10);
+        }
+
+        private void OffsetPlusSmall(object sender, RoutedEventArgs e)
+        {
+            OffsetMove(sender, e, 2);
+        }
+
+        private void OffsetMinusSmall(object sender, RoutedEventArgs e)
+        {
+            OffsetMove(sender, e, -2);
+        }
+
+        private void OffsetMove(object sender, RoutedEventArgs e, int value)
+        {
+            OffsetBox.Text = (int.Parse(OffsetBox.Text) + value).ToString();
+            Reload(sender, e);
+            ApplySpecific(false);
+        }
+    }
+}
---
+++ Tools/ScanCleaner/SimpleBitmap.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ScanCleaner
+{
+    class SimpleBitmap
+    {
+        public SimpleBitmap(int width, int height)
+        {
+            BaseBitmap = new WriteableBitmap(width, height, 96, 96, PixelFormats.Bgr32, null);
+        }
+
+        public SimpleBitmap(BitmapSource source)
+        {
+            BaseBitmap = new WriteableBitmap(source);
+        }
+        
+        public WriteableBitmap BaseBitmap { get; private set; }
+
+        public unsafe uint GetPixel(int x, int y)
+        {
+            var ptr = GetBackBufferPtr(x, y);
+            switch (BaseBitmap.Format.BitsPerPixel)
+            {
+                case 8:
+                    return *ptr;
+                case 16:
+                    return *(ushort*) ptr;
+                case 32:
+                    return *(uint*)ptr;
+                default:
+                    throw new DataMisalignedException("Misaligned Bits per pixel. 8, 16 and 32 supported.");
+            }
+        }
+
+        public void SetPixel(int x, int y, uint c)
+        {
+            BaseBitmap.Lock();
+            SetPixelUnlocked(x, y, c);
+            BaseBitmap.AddDirtyRect(new Int32Rect(x, y, 1, 1)); 
+            BaseBitmap.Unlock();
+        }
+
+        private unsafe void SetPixelUnlocked(int x, int y, uint c)
+        {
+            var ptr = GetBackBufferPtr(x, y);
+            switch (BaseBitmap.Format.BitsPerPixel)
+            {
+                case 8:
+                    *ptr = (byte) c;
+                    break;
+                case 16:
+                    *(ushort*) ptr = (ushort) c;
+                    break;
+                case 32:
+                    *(uint*) ptr = c;
+                    break;
+                default:
+                    throw new DataMisalignedException("Misaligned Bits per pixel. 8, 16 and 32 supported.");
+            }
+        }
+
+        private unsafe byte* GetBackBufferPtr(int x, int y)
+        {
+            const int bitsPerByte = 8;
+            var byteOffset = BaseBitmap.BackBufferStride * y + x * (BaseBitmap.Format.BitsPerPixel / bitsPerByte);
+            return ((byte*) BaseBitmap.BackBuffer.ToPointer()) + byteOffset;
+        }
+
+        public Color GetColor(int x, int y)
+        {
+            var p = BitConverter.GetBytes(GetPixel(x, y));
+            return Color.FromArgb(p[3], p[2], p[1], p[0]);
+        }
+
+        public void SetColor (int x, int y, Color c, bool perfromLock)
+        {
+            var color = (uint) c.A << 24 | ((uint) c.R << 16) | ((uint) c.G << 8) | c.B;
+            if (perfromLock)
+                SetPixel(x, y, color);
+            else
+                SetPixelUnlocked(x, y, color);
+        }
+    }
+}
